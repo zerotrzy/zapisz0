ff0e2b5853d3ad3c27fa04764dfbae57e60bad90
kermit <70634364+zerotrzy@users.noreply.github.com>
2020-09-05 14:08:46 +0200

    logi

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const electron = require('electron');
 const path = require('path');
 const fs = require('fs');
-const { dialog } = require('electron')
+const dialog = require('electron')
+
+console.log(dialog)
 
 var save = document.getElementById('save');
+
 
 save.addEventListener('click', (event) => {
     // Resolves to a Promise<Object>
     dialog.showSaveDialog({
         title: 'Select the File Path to save',
-        defaultPath: path.join(__dirname, '../sample.txt'),
+        defaultPath: path.join(__dirname, './sample.txt'),
         buttonLabel: 'Save',
         // Restricting the user to only Text Files.
         filters: [
@@ -19,18 +22,18 @@ save.addEventListener('click', (event) => {
             }, ],
         properties: []
     }).then(file => {
-        // Stating whether dialog operation was cancelled or not. 
-        console.log(file.canceled); 
-        if (!file.canceled) { 
+        // Stating whether dialog operation was cancelled or not.
+        console.log(file.canceled);
+        if (!file.canceled) {
             console.log(file.filePath.toString());
-            // Creating and Writing to the sample.txt file 
+            // Creating and Writing to the sample.txt file
             fs.writeFile(file.filePath.toString(),
-                         'This is a Sample File', function (err) { 
-                if (err) throw err; 
-                console.log('Saved!'); 
-            }); 
-        } 
-    }).catch(err => { 
-        console.log(err) 
-    }); 
+                         'This is a Sample File', function (err) {
+                if (err) throw err;
+                console.log('Saved!');
+            });
+        }
+    }).catch(err => {
+        console.log(err)
+    });
 });
